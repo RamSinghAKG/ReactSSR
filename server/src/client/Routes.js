cb2bb63e74f1d4facad90203c52e5196d92e8025
Ram Singh <ram.singh@zivame.com>
2017-11-17 10:52:50 +0530

    WIP error handling-83

--- server/src/client/Routes.js
+++ server/src/client/Routes.js
@@ -4,6 +4,7 @@ import App from './App';
 import HomePage from './pages/HomePage';
 import UsersListPage from './pages/UsersListPage';
 import NotFoundPage from './pages/NotFoundPage';
+import AdminsListPage from './pages/AdminListPage';
 export default [
   {
     ...App,
@@ -16,6 +17,10 @@ export default [
       {
         ...UsersListPage,
         path: '/users'
+      },
+      {
+        ...AdminsListPage,
+        path: '/admins'
       },
       {
         ...NotFoundPage
--- server/src/client/actions/index.js
+++ server/src/client/actions/index.js
@@ -1,13 +1,28 @@
 // import axios from 'axios';
 
 export const FETCH_USERS = 'fetch_users';
-export const fetchUsers = () => async (dispatch, getState, axiosInstance) => {
+export const fetchUsers = () => async(dispatch, getState, axiosInstance) => {
     const res = await axiosInstance.get('/users');
-    dispatch({type: FETCH_USERS, payload: res});
-}
+    dispatch({
+        type: FETCH_USERS,
+        payload: res
+    });
+};
 
 export const FETCH_CURRENT_USER = 'fetch_current_user';
-export const fetchCurrentUser = () => async (dispatch, getState, api) => {
+export const fetchCurrentUser = () => async(dispatch, getState, api) => {
     const res = await api.get('/current_user');
-    dispatch({type: FETCH_CURRENT_USER, payload: res});
-}
+    dispatch({
+        type: FETCH_CURRENT_USER,
+        payload: res
+    });
+};
+
+export const FETCH_ADMINS = 'fetch_admins';
+export const fetchAdmins = () => async(dispatch, getState, api) => {
+    const res = await api.get('/admins');
+    dispatch({
+        type: FETCH_ADMINS,
+        payload: res
+    });
+};
